877b92851f9e14030ab1c329f83ce383e1a28b27
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-01-20 21:00:26 -0500

    variables_constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,21 @@
+var nombre = "Miguel";
+let edad = 23;
+
+var profesor = true;
+var lista_cursos = ['JavaScript', 'PHP'];
+
+var informacio_personal = { 
+    id: 10248658,
+    dirrecion: "Calle 11",
+};
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacio_personal);
+
+
+const cedula = 14615
+
+console.warn(cedula);
